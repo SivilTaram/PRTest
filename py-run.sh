2bdf01f06315761d8ff43cbc21066bc9ce2df20c
SivilTaram <qianlxc@outlook.com>
2018-10-08 00:41:37 +0800

    update python script

--- py-run.sh
+++ py-run.sh
@@ -2,9 +2,10 @@
 set -ev
 dir=$(ls -l | grep ^d | awk '/^d/ {print i$NF}' i=`pwd`'/')
 for i in $dir
-    do 
-        mes=`python3 check.py $i`
-        if [ -n "$mes" ]
-        then curl -H "Authorization: token ${GITHUB_TOKEN}" -X POST -d "{\"body\": \"$mes\"}" "https://api.github.com/repos/${TRAVIS_REPO_SLUG}/issues/${TRAVIS_PULL_REQUEST}/comments"
+do 
+    mes=`python3 check.py $i`
+    if [ "$TRAVIS_PULL_REQUEST" != "false" ]
+    # just pull request get comments
+    then curl -H "Authorization: token ${GITHUB_TOKEN}" -X POST -d "{\"body\": \"$mes\"}" "https://api.github.com/repos/${TRAVIS_REPO_SLUG}/issues/${TRAVIS_PULL_REQUEST}/comments"
     fi
 done
