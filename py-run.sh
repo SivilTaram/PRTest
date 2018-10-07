1c5b9432d68c99be1aa8e17a6415b4e197b866bb
SivilTaram <qianlxc@outlook.com>
2018-10-08 00:54:13 +0800

    Fix

--- py-run.sh
+++ py-run.sh
@@ -6,6 +6,8 @@ do
     mes=`python3 check.py $i`
     if [ "$TRAVIS_PULL_REQUEST" != "false" ]
     # just pull request get comments
-    then curl -H "Authorization: token ${GITHUB_TOKEN}" -X POST -d "{\"body\": \"$mes\"}" "https://api.github.com/repos/${TRAVIS_REPO_SLUG}/issues/${TRAVIS_PULL_REQUEST}/comments"
+    then
+    echo "Begin Comment"
+    curl -H "Authorization: token $GITHUB_TOKEN" -X POST -d "{\"body\": \"$mes\"}" "https://api.github.com/repos/$TRAVIS_REPO_SLUG/issues/$TRAVIS_PULL_REQUEST/comments"
     fi
 done
