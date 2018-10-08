8e741cf824f031bc8c2a640797480cc5fe60f0ce
SivilTaram <qianlxc@outlook.com>
2018-10-08 15:19:46 +0000

    Add commit hint for user experience

--- py-run.sh
+++ py-run.sh
@@ -1,13 +1,22 @@
 #!/bin/bash
 set -ev
+logfile=log.txt
 dir=$(ls -l | grep ^d | awk '/^d/ {print i$NF}' i=`pwd`'/')
 for i in $dir
 do 
-    mes=`python3 check.py $i`
+    exitCode=`python3 check.py $i $logfile`
+    logContent=$(cat $logfile)
+    lastCommit=`curl -X GET https://api.github.com/repos/$TRAVIS_REPO_SLUG/pulls/$TRAVIS_PULL_REQUEST/commits | jq .[-1].sha -r`
+    if [ exitCode = 0 ]
+    then
+    buildStatus="Success"
+    else
+    buildStatus="Fail"
+    fi
     if [ "$TRAVIS_PULL_REQUEST" != "false" ]
     # just pull request get comments
     then
-    echo "Begin Comment"
-    curl -H "Authorization: token $GITHUB_TOKEN" -X POST -d "{\"body\": \"$mes\", \"commit_id\":\"\", \"path\":\"\", \"position\":\"\"}" "https://api.github.com/repos/$TRAVIS_REPO_SLUG/pulls/$TRAVIS_PULL_REQUEST/comments"
+    curl -H "Authorization: token $GITHUB_TOKEN" -X POST -d "{\"body\": \"**Commit**:$lastCommit  \n\n**Build Status**:  $buildStatus\n\n**Detail**:  $logContent\"}" "https://api.github.com/repos/$TRAVIS_REPO_SLUG/issues/$TRAVIS_PULL_REQUEST/comments"
     fi
+    exit $exitCode
 done
