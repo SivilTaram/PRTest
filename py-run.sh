10f5c5e1a0c114ae331fd9a0fdbead22fd372f5e
SivilTaram <qianlxc@outlook.com>
2018-10-08 15:43:36 +0000

    Mimal case for CI auto test

--- py-run.sh
+++ py-run.sh
@@ -6,7 +6,7 @@ for i in $dir
 do 
     exitCode=`python3 check.py $i $logfile`
     logContent=$(cat $logfile)
-    lastCommit=`curl -X GET https://api.github.com/repos/$TRAVIS_REPO_SLUG/pulls/$TRAVIS_PULL_REQUEST/commits | jq .[-1].sha -r`
+    lastCommit=`curl -X GET https://api.github.com/repos/$TRAVIS_REPO_SLUG/pulls/$TRAVIS_PULL_REQUEST/commits | jq '.[-1].sha' -r`
     if [ exitCode == 0 ]
     then
     buildStatus="Success"
@@ -18,4 +18,5 @@ do
     then
     curl -H "Authorization: token $GITHUB_TOKEN" -X POST -d "{\"body\": \"**Commit**:$lastCommit  \n\n**Build Status**:  $buildStatus\n\n**Detail**:  $logContent\"}" "https://api.github.com/repos/$TRAVIS_REPO_SLUG/issues/$TRAVIS_PULL_REQUEST/comments"
     fi
+    exit $exitCode
 done
