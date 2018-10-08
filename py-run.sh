d68ee71486b504f8893f2e607ff5d6a697e9f02d
SivilTaram <qianlxc@outlook.com>
2018-10-08 09:37:54 +0000

    Add modification on pull request comment

--- py-run.sh
+++ py-run.sh
@@ -8,6 +8,6 @@ do
     # just pull request get comments
     then
     echo "Begin Comment"
-    curl -H "Authorization: token $GITHUB_TOKEN" -X POST -d "{\"body\": \"$mes\"}" "https://api.github.com/repos/$TRAVIS_REPO_SLUG/issues/$TRAVIS_PULL_REQUEST/comments"
+    curl -H "Authorization: token $GITHUB_TOKEN" -X POST -d "{\"body\": \"$mes\", \"commit_id\":\"\", \"path\":\"\", \"position\":\"\"}" "https://api.github.com/repos/$TRAVIS_REPO_SLUG/pulls/$TRAVIS_PULL_REQUEST/comments"
     fi
 done
