ffa821c706002092d5a035a8d5d8907db1935a4c
SivilTaram <qianlxc@outlook.com>
2018-10-09 01:04:08 +0000

    Fix bug in get last commit

--- py-run.sh
+++ py-run.sh
@@ -2,12 +2,14 @@
 set -v
 logfile=log.txt
 dir=$(ls -l | grep ^d | awk '/^d/ {print i$NF}' i=`pwd`'/')
+curl -X GET "https://api.github.com/repos/$TRAVIS_REPO_SLUG/pulls/$TRAVIS_PULL_REQUEST/commits" > commit.log
+lastCommit=`cat commit.log | jq '.[-1].sha' -r`
 shellSuccess=0
+
 for i in $dir
 do
   exitCode=`python3 check.py $i $logfile`
   logContent=$(cat $logfile)
-  lastCommit=`curl -X GET https://api.github.com/repos/$TRAVIS_REPO_SLUG/pulls/$TRAVIS_PULL_REQUEST/commits | jq '.[-1].sha' -r`
   if [ exitCode == 0 ]
   then
   buildStatus="Success"
